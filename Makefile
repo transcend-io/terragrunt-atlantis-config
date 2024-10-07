99b83208412a674e8c41ed4478214f86b78413ce
Almenon <almenon214@gmail.com>
2024-10-06 21:29:06 -0700

    1.19 release (#353)

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=1.18.0
+VERSION=1.19.0
 PATH_BUILD=build/
 FILE_COMMAND=terragrunt-atlantis-config
 FILE_ARCH=$(shell go env GOOS)_$(shell go env GOARCH)
--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "github.com/transcend-io/terragrunt-atlantis-config/cmd"
 // This variable is set at build time using -ldflags parameters.
 // But we still set a default here for those using plain `go get` downloads
 // For more info, see: http://stackoverflow.com/a/11355611/483528
-var VERSION string = "1.18.0"
+var VERSION string = "1.19.0"
 
 func main() {
 	cmd.Execute(VERSION)
